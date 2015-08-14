f1f46727a678d8cf09c2dc8c7295de155ad6837d
Enric Florit <efz1005@gmail.com>
2015-08-14 19:45:32 +0200

    New features: multiple types, array of ranged numbers, array of set elements

--- src/Structure/ArrayS.php
+++ src/Structure/ArrayS.php
@@ -18,6 +18,8 @@ class ArrayS extends Structure {
 
     protected $minimumItemNumber = 0;
 
+    protected $containsNullItems = null;
+
     public function __construct($data = null, $null = false) {
         parent::__construct("array", $data, $null);
     }
@@ -35,41 +37,54 @@ class ArrayS extends Structure {
                 return;
             }
 
-            // /^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]$/ -> class name, provided by php
-            $class = '[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*';
+            $sqBrackets = '\[(\d+|\d*\+|\*)?\]';
 
-            $types = '/^('  . $class . '|\*)?\[\]$/';
-            $typesNumber = '/^('  . $class . '|\*)?\[(\d+)\]$/';
-            $typesSymbol = '/^(' . $class . '|\*)?\[((\d+)?\+|\*)\]$/';
-            if (preg_match($types, $format)) {
-                $this->format = str_replace("[]", "", $format);
-            } else if (preg_match($typesNumber, $format)) {
-                $split = explode("[", $format);
-                $type = $split[0];
-                $n = str_replace("]", "", $split[1]);
+            $singleType = '/^([^\|]*)(' . $sqBrackets . ')$/';
+            $multipleType = '/^\((.*)\)(' . $sqBrackets . ')$/';
 
-                $this->format = array_fill(0, $n, $type);
-            } else if (preg_match($typesSymbol, $format)) {
-                $split = explode("[", $format);
-                // set $format to the type
-                $this->format = $split[0];
-
-                $itemNumber = substr($split[1], 0, strlen($split[1]) - 1);
-                switch ($itemNumber[0]) {
-                    case '+':
-                        $this->minimumItemNumber = 1;
-                        break;
-                    case '*':
-                        $this->minimumItemNumber = 0;
-                        break;
-                    default: // a number
-                        $this->minimumItemNumber = intval($itemNumber);
-                        break;
-                }
+            if (preg_match($singleType, $format, $matches) || preg_match($multipleType, $format, $matches)) {
+                $this->format = $matches[1];
+                $this->parseArraySqBrackets($matches[2]);
+            } else {
+                $this->format = $format;
             }
 
             if ($this->format === "") $this->format = "*";
         }
+    }
+
+    /**
+     * Parses expressions matching /\[(\d+|\d*\+|\*)?\]/
+     *
+     * @param string $sqBrackets
+     */
+    protected function parseArraySqBrackets($sqBrackets) {
+        $length = strlen($sqBrackets);
+        if ($sqBrackets === '[]' || $sqBrackets === '[*]') {
+            $this->minimumItemNumber = 0;
+        } else if ($sqBrackets === '[+]') {
+            $this->minimumItemNumber = 1;
+        } else if ($length > 2 && $sqBrackets[$length - 2] === '+') {// [n+] case
+            $this->minimumItemNumber = substr($sqBrackets, 1, -2);
+        } else {// [n] case
+            $count = substr($sqBrackets, 1, -1);
+            $this->format = array_fill(0, intval($count), $this->format);
+        }
+    }
+
+
+    protected function searchNullItems() {
+        if (is_null($this->containsNullItems)) {
+            $this->containsNullItems = false;
+            foreach ($this->format as $item) {
+                if (is_string($item) && strpos($item, "null") !== false) {
+                    $this->containsNullItems = true;
+                    break;
+                }
+            }
+        }
+
+        return $this->containsNullItems;
     }
 
     /**
@@ -160,8 +175,19 @@ class ArrayS extends Structure {
             return $this->getNull() ? true : count($data) >= $this->minimumItemNumber;
         }
 
-        if (!$this->getNull() && $this->isCountStrict() && count($data) !== count($this->format)){
-            return false;
+        /**
+         * countStrict behavior:
+         *  - if $data has more entries than the $format, it's up to $countStrict
+         *  - if $data has the same entries as the $format, ok
+         *  - if $data has less entries than the $format, the string "null" is searched
+         *      in $format
+         */
+        if (!$this->getNull() && $this->isCountStrict()){
+            if (count($data) > count($this->format) ||
+                (count($data) < count($this->format) && !$this->searchNullItems())) {
+                return false;
+            }
+            // else: ok
         }
 
         $associativeData = ArrayS::isAssociative($data);
@@ -169,11 +195,8 @@ class ArrayS extends Structure {
 
         if ($associativeData && $associativeFormat) {
             foreach ($this->getFormat() as $key=>$value) {
-                if (!array_key_exists($key, $data)) {
-                    $valid = $this->getNull();
-                } else {
-                    $valid = $this->checkValue($data[$key], $value);
-                }
+                $testData = array_key_exists($key, $data) ? $data[$key] : null;
+                $valid = $this->checkValue($testData, $value);
                 if (!$valid) return false;
             }
             return true;
@@ -192,18 +215,62 @@ class ArrayS extends Structure {
      * @param mixed $data
      * @param mixed $format
      * @param bool $applyFormat
-     * @return bool
+     * @return bool|array
      * @throws \Exception
      */
     protected function checkValue($data, $format, $applyFormat = false) {
-        $numeric = '/^(numeric|float|integer|int)(\(|\[).+,.+(\)|\])$/';
-        $valueSetScalar = '/^(scalar|string|float|integer|int|str|boolean|bool|numeric)\{[^}]*\}$/';
-
-        if (is_null($data)) {
+        if (is_null($data) && !$this->searchNullItems()) {
             $valid = $this->getNull();
         } else if (is_string($format)) {
-            if (preg_match($numeric, $format)) {
-                switch ($format[0]) {
+            return $this->checkValueStringFormat($data, $format, $applyFormat);
+        } else if (is_array($format)) {
+            $a = new ArrayS($data, $this->getNull());
+            $a->setCountStrict($this->countStrict);
+            $a->setFormat($format);
+            $valid = $a->check();
+        } else {
+            $valid = true;
+        }
+
+        return $valid;
+    }
+
+    /**
+     * @param mixed $data
+     * @param string $format
+     * @param bool|false $applyFormat
+     * @return bool|array
+     * @throws \Exception
+     */
+    protected function checkValueStringFormat($data, $format, $applyFormat = false) {
+        /**
+         * Examples:
+         *  numeric[-10, 30)
+         *  integer(0, inf)
+         *  float[0,1]
+         */
+        $numeric = '/^(numeric|float|integer|int)(\(|\[).+,.+(\)|\])$/';
+        /**
+         * Examples:
+         *  string{pendent, payed, ready, cancelled}
+         *  integer{10, 100, 1000}
+         *  scalar{0, false}
+         */
+        $valueSetScalar = '/^(scalar|string|float|integer|int|str|boolean|bool|numeric)\{[^}]*\}$/';
+
+
+        // allow "type1|type2|..."
+        $types = explode("|", $format);
+        if (count($types) > 1 && $applyFormat) {
+            throw new \Exception("Can't format \$data to multiple types");
+        }
+
+        // Use a do-while to check always $type[0]
+        $i = 0;
+        do {
+            $type = $types[$i];
+            if (preg_match($numeric, $type)) {
+                switch ($type[0]) {
                     case "n":
                         $structure = new NumericS();
                         break;
@@ -216,14 +283,14 @@ class ArrayS extends Structure {
                 }
                 /** @var NumericS $structure */
                 $structure->setNull($this->getNull());
-                $structure->setRange(preg_replace("/^(numeric|float|integer)/", "", $format));
+                $structure->setRange(preg_replace("/^(numeric|float|integer)/", "", $type));
                 if ($applyFormat) {
                     return $structure->format($data);
                 } else {
                     $valid = $structure->check($data);
                 }
-            } else if (preg_match($valueSetScalar, $format)) {
-                switch ($format[0] . $format[1]) {
+            } else if (preg_match($valueSetScalar, $type)) {
+                switch ($type[0] . $type[1]) {
                     default:
                     case "sc":
                         $structure = new ScalarS();
@@ -246,64 +313,68 @@ class ArrayS extends Structure {
                 }
                 /** @var ScalarS $structure */
                 $structure->setNull($this->getNull());
-                $structure->setValueSet(preg_replace('/^[^{]+/', "", $format));
+                $structure->setValueSet(preg_replace('/^[^{]+/', "", $type));
                 if ($applyFormat) {
                     return $structure->format($data);
                 } else {
                     $valid = $structure->check($data);
                 }
             } else {
-                switch ($format) {
-                    case "scalar":
-                        $structure = new ScalarS();
-                        break;
-                    case "string":
-                    case "str":
-                        $structure = new StringS();
-                        break;
-                    case "numeric":
-                        $structure = new NumericS();
-                        break;
-                    case "integer":
-                    case "int":
-                        $structure = new IntegerS();
-                        break;
-                    case "float":
-                        $structure = new FloatS();
-                        break;
-                    case "boolean":
-                    case "bool":
-                        $structure = new BooleanS();
-                        break;
-                    case "array":
-                        $structure = new ArrayS();
-                        $structure->setFormat("array");
-                        $structure->setCountStrict($this->countStrict);
-                        break;
-                    case "*":
-                    case "any":
+                switch ($type) {
+                case "scalar":
+                    $structure = new ScalarS();
+                    break;
+                case "string":
+                case "str":
+                    $structure = new StringS();
+                    break;
+                case "numeric":
+                    $structure = new NumericS();
+                    break;
+                case "integer":
+                case "int":
+                    $structure = new IntegerS();
+                    break;
+                case "float":
+                    $structure = new FloatS();
+                    break;
+                case "boolean":
+                case "bool":
+                    $structure = new BooleanS();
+                    break;
+                case "array":
+                    $structure = new ArrayS();
+                    $structure->setFormat("array");
+                    $structure->setCountStrict($this->countStrict);
+                    break;
+                case "*":
+                case "any":
+                    if ($applyFormat) return $data;
+                    $valid = true;
+                    break;
+                case "null":
+                    if ($applyFormat) return null;
+                    $valid = is_null($data);
+                    break;
+                default:
+                    if (class_exists($format)) {
                         if ($applyFormat) return $data;
-                        $valid = true;
-                        break;
-                    default:
-                        if (class_exists($format)) {
-                            if ($applyFormat) return $data;
-                            $valid = $data instanceof $format;
-                        } else {
-                            // maybe $format is a simple array (type[] or type[int])
-                            $structure = new ArrayS();
-                            $structure->setFormat($format);
-                            $structure->setCountStrict($this->countStrict);
-                        }
-                        break;
+                        $valid = $data instanceof $format;
+                    } else {
+                        // maybe $format is a simple array (type[] or type[int])
+                        $structure = new ArrayS();
+                        $structure->setFormat($format);
+                        $structure->setCountStrict($this->countStrict);
+                    }
+                    break;
                 }
                 /** @var Structure $structure */
-                if (isset($structure)){
+                if (isset($structure)) {
                     $structure->setNull($this->getNull());
 
                     if ($applyFormat) {
                         return $structure->format($data);
-                    } else if (!isset($valid)) {
+                    } else if (!isset($valid) || !$valid) {
                         try {
                             $valid = $structure->check($data);
                         } catch (\Exception $e) {
@@ -314,14 +385,7 @@ class ArrayS extends Structure {
                     $valid = true;
                 }
             }
-        } else if (is_array($format)) {
-            $a = new ArrayS($data, $this->getNull());
-            $a->setCountStrict($this->countStrict);
-            $a->setFormat($format);
-            $valid = $a->check();
-        } else {
-            $valid = true;
-        }
+        } while(!$valid and count($types) > ++$i);
 
         return $valid;
     }
